25acbc7c015aa4bc45da9beee924cb2c7d2db0aa
marcelonv011 <marcelonv011@gmail.com>
2023-04-25 22:00:34 -0300

    adicionando controller (get, put, post)

--- src/main/java/br/com/uniamerica/estacionamento/estacionamentoapi/controller/ModeloController.java
+++ src/main/java/br/com/uniamerica/estacionamento/estacionamentoapi/controller/ModeloController.java
@@ -4,6 +4,7 @@ package br.com.uniamerica.estacionamento.estacionamentoapi.controller;
 import br.com.uniamerica.estacionamento.estacionamentoapi.entity.Modelo;
 import br.com.uniamerica.estacionamento.estacionamentoapi.repository.ModeloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,18 +20,73 @@ public class ModeloController {
     private ModeloRepository modeloRepository;
 
     @GetMapping("{id}")
-    public ResponseEntity<Optional <Modelo>> findAll(
+    public ResponseEntity<?> findByIdPath(
             @PathVariable("id") final long id
     ){
-        return ResponseEntity.ok().body(this.modeloRepository.findById(id));
+        final Modelo modelo = this.modeloRepository.findById(id).orElse(null);
+        return modelo == null
+                ? ResponseEntity.badRequest().body("Ningun valor encontrado.")
+                : ResponseEntity.ok(modelo);
     }
 
     @GetMapping
+    public ResponseEntity<?> findByIdRequest(
+            @RequestParam("id") final long id
+    ){
+        final Modelo modelo = this.modeloRepository.findById(id).orElse(null);
+        return modelo == null
+                ? ResponseEntity.badRequest().body("Ningun valor encontrado.")
+                : ResponseEntity.ok(modelo);
+    }
+
+    @GetMapping("lista")
+    public ResponseEntity<?> listaCompleta(){
+        return ResponseEntity.ok(this.modeloRepository.findAll());
+    }
+
+    @PostMapping
+    public  ResponseEntity<?> cadastrar(@RequestBody final Modelo modelo){
+        try {
+            this.modeloRepository.save(modelo);
+            return ResponseEntity.ok().body("Registro adicionado con exito");
+        }
+        catch (Exception e){
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> atualizar(
+            @RequestParam("id") final Long id,
+            @RequestBody final Modelo modelo
+    ) {
+        try {
+            final Modelo modeloBanco = this.modeloRepository.findById(id).orElse(null);
+
+            if (modeloBanco == null || !modeloBanco.getId().equals(modelo.getId())){
+                throw new RuntimeException("nao foi possivel identificar o registro informado.");
+            }
+
+            this.modeloRepository.save(modelo);
+            return ResponseEntity.ok("Registro atualizado com sucesso");
+        }
+        catch (DataIntegrityViolationException e){
+            return ResponseEntity.internalServerError()
+                    .body("Error:" + e.getCause().getCause().getMessage());
+        }
+        catch (RuntimeException e){
+            return ResponseEntity.internalServerError().body("error:" + e.getMessage());
+        }
+    }
+
+    }
+
+    /**@GetMapping
 
     @PostMapping
 
     @PutMapping
 
     @DeleteMapping
+**/
 
-}
