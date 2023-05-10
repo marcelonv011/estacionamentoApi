3573fd5c1678558a79a572d387cdf6d827205585
marcelonv011 <marcelonv011@gmail.com>
2023-05-09 21:24:36 -0300

    alterando enum cor

--- src/main/java/br/com/uniamerica/estacionamento/estacionamentoapi/entity/Cor.java
+++ src/main/java/br/com/uniamerica/estacionamento/estacionamentoapi/entity/Cor.java
@@ -1,10 +1,18 @@
 package br.com.uniamerica.estacionamento.estacionamentoapi.entity;
 
 public enum Cor {
-    BRANCO,
-    PRETO,
-    CINZA,
-    VERMELHO,
-    AZUL,
-    AMARELHO;
+    BRANCO(1,"Branco"),
+    PRETO(2, "Preto"),
+    CINZA(3,"Cinza"),
+    VERMELHO(4,"Vermelho"),
+    AZUL(5,"Azul"),
+    AMARELHO(6,"Amarelho");
+
+    public final int valorNum;
+    public final String valor;
+
+    private Cor(int valorNum,String valor){
+        this.valorNum = valorNum;
+        this.valor = valor;
+    }
 }
